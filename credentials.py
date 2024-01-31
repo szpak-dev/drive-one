3668e249860ba3e9f0222f725fcf42843a3e4f9e
Tomasz Szpak <tomszp@gmail.com>
2024-01-31 21:58:10 +0100

    fix

--- credentials.py
+++ credentials.py
@@ -1,5 +1,12 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Credentials:
     def __init__(self):
-        self.client_id = "fb4ad100-3dd4-4448-8216-fe90f31d94b1"
-        self.client_secret = "bVF~Y6oikClRjcDN1PKr6yxyS5BjOgj_KS"
-        self.tenant_id = "acbf9871-5f6f-4cff-b2ec-a4a764e86423"
+        self.client_id = getenv('CLIENT_ID')
+        self.client_secret = getenv('CLIENT_SECRET')
+        self.tenant_id = getenv('TENANT_ID')
